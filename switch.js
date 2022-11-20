d5c7679994e0e2287f2c8961f4164daf71509959
twc <tommy.wijaya.c@gmail.com>
2022-11-21 00:25:39 +0700

    theme switch impl

---
+++ switch.js
@@ -0,0 +1,23 @@
+const themeSwitch = document.querySelector('.form-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    themeSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }    
+}
+
+themeSwitch.addEventListener('change', switchTheme, false);
